2793c2dcf94c2dd4c0211c37a36ac1773bcbd8c0
krishnaSingh26 <krishaanasinghh@gmail.com>
2026-01-18 14:52:39 +0530

    code change

--- collectionsFramework/Collections.java
+++ collectionsFramework/Collections.java
@@ -1,5 +1,6 @@
+package collectionsFramework;
 public class Collections {
     public static void main(String[] args) {
-       System.err.println("hello world");
+       System.err.println("hello world 2 ");
     }
 }
